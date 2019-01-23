09e344dd2a99ab080a888499dd518b5154a02b7f
Anton <anton_pycarelle@hotmail.com>
2019-01-24 00:51:48 +0100

    HOMEPAGE 80% - Nog opt-in en feed

--- src/view/players/index.php
+++ src/view/players/index.php
@@ -10,73 +10,115 @@
   <a class="search" href=""><img src="./assets/img/glass.svg" alt="" width="29,07" height="29,07"></a>
 
   <div class="logo" >
-    <p>
-    <span>Internationaal</span><br>
-    <span>Straatthearer Fest<span>i</span>val</span><br>
-    <span>Beveren</span>
+    <p class="logo-title">
+    <span class="logo_int">Internationaal</span><br>
+    <span>Straattheater Fest<span class="accentkleur">i</span>val</span><br>
+    <span class="logo_int accentkleur a-right">Beveren</span>
     </p>
   </div>
 
-    <p class="datum">
-      Augustus <br>
+  <div class="datum">
+    <p>
+      <span class="datum_maand">Augustus</span> <br>
       23 - 24 - 25
     </p>
+  </div>
 
-    <a class="arrow" href=""><img src="./assets/img/arrow.svg" alt="" height="66,58" width="64,24"></a>
+    <a class="arrow" href="#content"><img src="./assets/img/arrow.svg" alt="" height="66,58" width="64,24"></a>
 
 </header>
-
-<main>
+<hr class="divider">
+<main id="content">
   <section>
-    <h2>Uitgelicht</h2>
-    <div>
-        <article>
-          <p>Vrijdag 20:30</p>
-          <h3>Title</h3>
+    <h2 class="tussentitle">Uitgelicht</h2>
+    <div class="uitgelicht-grid-container">
+    <article class="uitgelicht-big act-grid-container">
+        <img class="act-pic" src="./assets/img/straattheater.jpg" alt="lol">
+        <a href="" class="act-link">
+          <div class="act-info">
+            <p class="act-info__dag">Vrijdag 20:30</p>
+            <h3 class="act-info__title bold" >Title</h3>
+          </div>
+          </a>
         </article>
-    </div>
-    <div>
-        <article>
-          <p>Vrijdag 20:30</p>
-          <h3>Title</h3>
+
+        <article class="uitgelicht-event1 act-grid-container">
+        <img class="act-pic" src="./assets/img/straattheater.jpg" alt="lol">
+        <a href="" class="act-link">
+          <div class="act-info">
+            <p class="act-info__dag">Vrijdag 20:30</p>
+            <h3 class="act-info__title bold" >Title</h3>
+          </div>
+          </a>
         </article>
-        <article>
-          <p>Vrijdag 20:30</p>
-          <h3>Title</h3>
+
+        <article class="uitgelicht-event2 act-grid-container">
+        <img class="act-pic" src="./assets/img/straattheater.jpg" alt="lol">
+        <a href="" class="act-link">
+          <div class="act-info">
+            <p class="act-info__dag">Vrijdag 20:30</p>
+            <h3 class="act-info__title bold" >Title</h3>
+          </div>
+          </a>
         </article>
-        <article>
-          <p>Vrijdag 20:30</p>
-          <h3>Title</h3>
+
+        <article class="uitgelicht-event3 act-grid-container">
+        <img class="act-pic" src="./assets/img/straattheater.jpg" alt="lol">
+        <a href="" class="act-link">
+          <div class="act-info">
+            <p class="act-info__dag">Vrijdag 20:30</p>
+            <h3 class="act-info__title bold" >Title</h3>
+          </div>
+          </a>
         </article>
     </div>
   </section>
 
-  <section>
-    <h2>
+  <section class="aftermovie">
+    <h2 class="tussentitle">
       Hier is alvast een aftermovie van 2017
-    </h2>
-    <iframe src="" frameborder="0"></iframe>
+    </h2
+    ><div class="aftermovie-movie">
+    <iframe  width="560" height="315" src="https://www.youtube.com/embed/lrlscBtuSUM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
+    </div>
 </section>
 
 <section>
-  <h3>#ISB2018</h3>
-  <p>Gebruik de hashtag en word gefeatured op onze website</p>
+  <h3 class="tussentitle">#ISB2018</h3>
+  <p class="insta-text">Gebruik de hashtag en word gefeatured op onze website</p>
+  <div class="insta-grid-container">
+  <!-- <img class="act-pic" src="./assets/img/straattheater.jpg" alt="lol">
+  <img class="act-pic" src="./assets/img/straattheater.jpg" alt="lol">
+  <img class="act-pic" src="./assets/img/straattheater.jpg" alt="lol">
+  <img class="act-pic" src="./assets/img/straattheater.jpg" alt="lol">
+  <img class="act-pic" src="./assets/img/straattheater.jpg" alt="lol">
+  <img class="act-pic" src="./assets/img/straattheater.jpg" alt="lol">
+  <img class="act-pic" src="./assets/img/straattheater.jpg" alt="lol">
+  <img class="act-pic" src="./assets/img/straattheater.jpg" alt="lol">
+  <img class="act-pic" src="./assets/img/straattheater.jpg" alt="lol">
+  <img class="act-pic" src="./assets/img/straattheater.jpg" alt="lol"> -->
+
+
+
+  </div>
 
 </section>
 </main>
 
-<footer>
-<nav>
+<footer class="footer">
+  <div class="footer-grid-container">
+<nav class="footer-nav nav">
     <ul>
-      <li><a class="nav-link-item active" href="">Home</a></li>
+      <li><a class="nav-link-item nav-link-item__active" href="">Home</a></li>
       <li><a class="nav-link-item" href="">Agenda</a></li>
       <li><a class="nav-link-item" href="">Praktisch</a></li>
       <li><a class="nav-link-item" href="">Extra</a></li>
     </ul>
   </nav>
-  <div>
-    <img src="" alt="">
+  <div class="footer-info">
+    <img src="./assets/img/logobeveren.png" width="203" height="40" alt="Beveren verbindt logo">
     <a href="">info@beveren.be</a>
     <a href="">0476 38 38 39</a>
+  </div>
   </div>
 </footer>
